064a569141361711b3b5cfbc970aad0a09fa4955
alburnus <k.ukleja@gmail.com>
2022-05-31 23:20:23 +0200

    Validation

---
+++ src/main/java/com/github/alburnus/javastuff/model/ContactDetail.java
@@ -0,0 +1,14 @@
+package com.github.alburnus.javastuff.model;
+
+import lombok.Data;
+
+/**
+ * Contact detail model
+ */
+@Data
+public class ContactDetail {
+
+    private String dialCode;
+    private String phoneNumber;
+    private String email;
+}
